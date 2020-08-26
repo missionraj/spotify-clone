c3af4404cdf9e25eba016cff6953cdbbe27159e6
raj <raj@coutloot.com>
2020-08-26 23:06:31 +0530

    login page made

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , { useEffect , useState }from 'react';
 import './App.css';
+import Login from './login/Login';
+
+import { getTokenFromResponse } from './spotify';
+
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify =  new SpotifyWebApi();
 
 function App() {
+  let [ token , setToken ] = useState('');
+  useEffect(()=>{
+      const hash = getTokenFromResponse();
+      const _token  = hash.access_token;
+      if (_token) {
+        setToken(_token);
+        spotify.setAccessToken(_token);
+        spotify.getMe().then(user=>{
+          console.log('user ==> ', user);
+        })
+      }
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {
+        token ? (
+          <h1>user is logged in </h1>
+        ) : (
+          <Login />
+        )
+      }
     </div>
   );
 }
---
+++ src/player/player.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function  Player() {
+    return (
+        <div>
+            <h1> i am logged in</h1>
+        </div>
+    )
+}
+
+export default Player
